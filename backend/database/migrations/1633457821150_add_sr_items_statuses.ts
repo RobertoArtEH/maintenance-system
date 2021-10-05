abd232f3ff99a0f3fbd07b25666a07784a562d4a
GerardoDM <geradelgado22@hotmail.com>
2021-10-05 13:23:20 -0500

    added status fields and isAdmin field

---
+++ backend/database/migrations/1633457821150_add_sr_items_statuses.ts
@@ -0,0 +1,15 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class ServiceRequestsItems extends BaseSchema {
+  protected tableName = 'service_requests_items'
+
+  public async up () {
+    this.schema.alterTable(this.tableName, (table) => {
+    })
+  }
+
+  public async down () {
+    this.schema.alterTable(this.tableName, (table) => {
+    })
+  }
+}
