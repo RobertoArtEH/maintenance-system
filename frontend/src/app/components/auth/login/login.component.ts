db516856bfe799275d10e4e5b058f3f6d1568ab9
Andrés Orozco <aestrada@lumston.com>
2021-10-09 21:30:34 -0500

    [ADD] Login

--- frontend/src/app/components/auth/login/login.component.ts
+++ frontend/src/app/components/auth/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/auth/authentication.service';
+import { errorMessage, successDialog } from '../../resources/alert';
 
 @Component({
   selector: 'app-login',
@@ -7,18 +10,43 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  registerForm: FormGroup
-  constructor(private fb: FormBuilder) { }
+  loginForm: FormGroup
+
+  constructor(private fb: FormBuilder, private authenticationService: AuthenticationService, private router: Router) { }
 
   ngOnInit(): void {
     this.buildForm()
   }
 
   buildForm(): void {
-    this.registerForm = this.fb.group({
+    this.loginForm = this.fb.group({
       email: ['',[Validators.required,Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')]],
       password: ['', [Validators.required]]
     });
   }
 
+  login(){
+    if (this.loginForm.valid){
+      this.authenticationService.login(this.loginForm.value).subscribe( res => {
+        this.authenticationService.setToken(res.token, res.token.refresh_token);
+        if (res.status){
+          successDialog('Bienvenido').then(() =>{
+            this.authenticationService.setToken(res.token.token, res.token.refresh_token);
+            return this.router.navigate(['/home']);
+          })
+        } else{
+          errorMessage('Error')
+        }
+      }, res => {
+        console.log(res)
+        errorMessage('El correo electrónico y/o contraseña son incorrectos.')
+      })  
+    } else{
+      errorMessage('Campos obligatorios')
+    } 
+  }
+
+  clearForm(){
+    this.loginForm.reset()
+  }
 }
